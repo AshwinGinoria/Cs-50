7efef645d62e2901ea3667adb952a57bc3007de3
AshwinGinoria <AshwinGinoria@users.noreply.github.com>
2019-08-19 12:47:32 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- vigenere.c
+++ vigenere.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
         return 1;
     }
     // Prints Usage if Invalid Keyword is Entered
-    else if ( isword(strlen(argv[1]), argv[1]) == 0 )
+    else if (isword(strlen(argv[1]), argv[1]) == 0)
     {
         printf("Usage: ./vigenere keyword\n");
         return 2;
@@ -23,16 +23,18 @@ int main(int argc, string argv[])
     int l = strlen(argv[1]) ;
     string keyword = argv[1] ;
     char plain_text[100] ;
+    // Taking Input text
     printf("plaintext: ");
-    scanf("%[^\n]s", plain_text );
+    scanf("%[^\n]s", plain_text);
     printf("ciphertext: ");
+    // Printing cipher text
     for (int i = 0, n = strlen(plain_text) ; i < n ; i++)
     {
-        if ( islower(plain_text[i]) )
+        if (islower(plain_text[i]))
         {
             printf("%c", (char)('a' + (plain_text[i] - 'a' + shift(keyword[i % l])) % 26)) ;
         }
-        else if( isupper(plain_text[i]))
+        else if (isupper(plain_text[i]))
         {
             printf("%c", (char)('A' + (plain_text[i] - 'A' + shift(keyword[i % l])) % 26));
         }
@@ -44,9 +46,10 @@ int main(int argc, string argv[])
     printf("\n") ;
 }
 
+// Function to check if the given word only consists of characters
 int isword(int length, char a[])
 {
-    for(int i = 0 ; i < length ; i++)
+    for (int i = 0 ; i < length ; i++)
     {
         if (!((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z')))
         {
@@ -56,10 +59,15 @@ int isword(int length, char a[])
     return 1;
 }
 
+// Function to determine and return equivalent shift
 int shift(char c)
 {
-    if ( c - 'a' >= 0)
+    if (c - 'a' >= 0)
+    {
         return (int)(c - 'a');
+    }
     else
+    {
         return (int)(c - 'A');
+    }
 }
